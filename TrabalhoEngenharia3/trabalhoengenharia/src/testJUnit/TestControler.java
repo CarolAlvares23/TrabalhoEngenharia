1c8e9dbc33603fd83aaaf59687793d86c2632af5
CarolAlvares23 <122477075+CarolAlvares23@users.noreply.github.com>
2024-02-17 15:11:30 -0300

    TestControler.java

--- TrabalhoEngenharia3/trabalhoengenharia/src/testJUnit/TestControler.java
+++ TrabalhoEngenharia3/trabalhoengenharia/src/testJUnit/TestControler.java
@@ -1,74 +1,60 @@
 package testJUnit;
 
 import projetoBiblioteca.*;
-
 import static org.junit.Assert.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class TestControler {
+    private static final String RA_VALIDO = "123";
+    private static final String RA_INVALIDO = "10";
+    private static final String RA_COM_DEBITO = "4";
+    private static final int[] PRAZOS_VALIDOS = {1,3,5,6};
+    private static final int[] PRAZOS_COM_LIVRO_NAO_VERIFICADO = {1,0,99};
+    private static final int[] PRAZOS_SO_COM_LIVROS_NAO_VERIFICADOS = {0,99};
+    private Controle controle;
 
-	@Test
-	void testNaoEmprestaLivrosParaAlunoNaoVerificado() {
-		String ra = "10";
-		Aluno aluno = new Aluno(ra);
-		assertFalse(aluno.verficaAluno());
+    @BeforeEach
+    void setUp() {
+        controle = new Controle();
+    }
 
-		int[] prazos = {1,2,3};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos.length, prazos);
-		assertFalse(retorno);
-	}
+    @Test
+    void testNaoEmprestaLivrosParaAlunoNaoVerificado() {
+        Aluno aluno = new Aluno(RA_INVALIDO);
+        assertFalse("O aluno não deveria ser verificado", aluno.verficaAluno());
+        assertFalse("Não deveria emprestar para aluno não verificado", controle.emprestar(RA_INVALIDO, PRAZOS_VALIDOS.length, PRAZOS_VALIDOS));
+    }
 
-	@Test
-	void testNaoEmprestaLivrosParaAlunoComDebito() {
-		String ra = "4";
-		Aluno aluno = new Aluno(ra);
-		assertFalse(aluno.verificaDebito());
+    @Test
+    void testNaoEmprestaLivrosParaAlunoComDebito() {
+        Aluno aluno = new Aluno(RA_COM_DEBITO);
+        assertFalse("O aluno não deveria ter débito", aluno.verificaDebito());
+        assertFalse("Não deveria emprestar para aluno com débito", controle.emprestar(RA_COM_DEBITO, PRAZOS_VALIDOS.length, PRAZOS_VALIDOS));
+    }
 
-		int[] prazos = {1,2,3};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos.length, prazos);
-		assertFalse(retorno);
-	}
+    @Test
+    void testNaoEmprestaLivroNaoVerificado() {
+        Livro livro0 = new Livro(1);
+        assertFalse("O livro 1 não deveria ser verificado", livro0.verificaLivro());
+        Livro livro1 = new Livro(0);
+        assertTrue("O livro 0 deveria ser verificado", livro1.verificaLivro());
+        Livro livro2 = new Livro(99);
+        assertTrue("O livro 99 deveria ser verificado", livro2.verificaLivro());
+        assertTrue("Deveria emprestar pelo menos 1 livro verificado", controle.emprestar(RA_VALIDO, PRAZOS_COM_LIVRO_NAO_VERIFICADO.length, PRAZOS_COM_LIVRO_NAO_VERIFICADO));
+    }
 
-	@Test
-	void testNaoEmprestaLivroNaoVerificado() {
-		String ra = "123";
-		int[] prazos = {1,0,99};  // 2 e 4 nao é verificado
-		Livro livro0 = new Livro(1);
-		assertTrue(!livro0.verificaLivro());
-		Livro livro1 = new Livro(0);
-		assertFalse(!livro1.verificaLivro());
-		Livro livro2 = new Livro(99);
-		assertFalse(!livro2.verificaLivro());
-		
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos.length, prazos);
-		assertTrue(retorno);  // emprestou só 1 livro
-	}
+    @Test
+    void testNaoEmprestaNenhumLivroNaoVerificado() {
+        Livro livro1 = new Livro(0);
+        assertTrue("O livro 0 deveria ser verificado", livro1.verificaLivro());
+        Livro livro2 = new Livro(99);
+        assertTrue("O livro 99 deveria ser verificado", livro2.verificaLivro());
+        assertFalse("Não deveria emprestar nenhum livro não verificado", controle.emprestar(RA_VALIDO, PRAZOS_SO_COM_LIVROS_NAO_VERIFICADOS.length, PRAZOS_SO_COM_LIVROS_NAO_VERIFICADOS));
+    }
 
-	@Test
-	void testNaoEmprestaNenhumLivroNaoVerificado() {
-		String ra = "123";
-		int[] prazos = {0,99};  // 2 e 4 nao é verificado
-		Livro livro1 = new Livro(0);
-		assertFalse(!livro1.verificaLivro());
-		Livro livro2 = new Livro(99);
-		assertFalse(!livro2.verificaLivro());
-		
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos.length, prazos);
-		assertFalse(retorno);
-	}
-
-	@Test
-	void testEmprestaLivros() {
-		String ra = "123";
-		int[] prazos = {1,3,5,6};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos.length, prazos);
-		assertTrue(retorno);
-	}
+    @Test
+    void testEmprestaLivros() {
+        assertTrue("Deveria emprestar todos os livros verificados", controle.emprestar(RA_VALIDO, PRAZOS_VALIDOS.length, PRAZOS_VALIDOS));
+    }
 }
